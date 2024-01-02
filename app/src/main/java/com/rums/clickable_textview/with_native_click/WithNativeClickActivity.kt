488ad57d5b02285cc317998db2adbfccf4d029fd
rumit <rumit@scopgenxdigital.com>
2024-01-02 13:31:34 +0530

    linecount calculated in Native click. activity

--- app/src/main/java/com/rums/clickable_textview/with_native_click/WithNativeClickActivity.kt
+++ app/src/main/java/com/rums/clickable_textview/with_native_click/WithNativeClickActivity.kt
@@ -3,6 +3,7 @@ package com.rums.clickable_textview.with_native_click
 import android.content.Context
 import android.os.Bundle
 import android.text.method.LinkMovementMethod
+import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.rums.clickable_textview.R
@@ -32,10 +33,31 @@ class WithNativeClickActivity : AppCompatActivity() {
         tvDescriptionReadMoreHide = findViewById(R.id.tvDescriptionReadMoreHide)
 
         enableDefaultClick()
+        checkLineCountAndSetVisibility()
     }
 
     private fun enableDefaultClick() { // Enable TextView's click
         tvOverview.movementMethod = LinkMovementMethod.getInstance()
         tvDescription.movementMethod = LinkMovementMethod.getInstance()
+    }
+
+    private fun checkLineCountAndSetVisibility() {
+        tvOverview.movementMethod = LinkMovementMethod.getInstance()
+        tvOverview.post {
+            if (tvOverview.lineCount > 3) {
+                tvOverviewReadMoreHide.visibility = View.VISIBLE
+            } else {
+                tvOverviewReadMoreHide.visibility = View.GONE
+            }
+        }
+
+        tvDescription.movementMethod = LinkMovementMethod.getInstance()
+        tvDescription.post {
+            if (tvDescription.lineCount > 3) {
+                tvDescriptionReadMoreHide.visibility = View.VISIBLE
+            } else {
+                tvDescriptionReadMoreHide.visibility = View.GONE
+            }
+        }
     }
 }
