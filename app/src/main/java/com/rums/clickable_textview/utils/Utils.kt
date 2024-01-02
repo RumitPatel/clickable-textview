15a31e58d09ff979231fe41c2f7d6d75ef88ade7
rumit <rumit@scopgenxdigital.com>
2024-01-02 13:52:28 +0530

    ClickableSpan is implemented with detecting single click.

---
+++ app/src/main/java/com/rums/clickable_textview/utils/Utils.kt
@@ -0,0 +1,10 @@
+package com.rums.clickable_textview.utils
+
+import android.content.Context
+import android.widget.Toast
+
+fun Context.toast(message: String?) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+class Utils {
+}
--- app/src/main/java/com/rums/clickable_textview/with_clickable_span/WithClickableSpanActivity.kt
+++ app/src/main/java/com/rums/clickable_textview/with_clickable_span/WithClickableSpanActivity.kt
@@ -1,14 +1,20 @@
-package com.rums.clickable_textview.custom_test
+package com.rums.clickable_textview.with_clickable_span
 
 import android.content.Context
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
 import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.rums.clickable_textview.R
+import com.rums.clickable_textview.utils.toast
 
-class WithCustomTestActivity : AppCompatActivity() {
+
+class WithClickableSpanActivity : AppCompatActivity() {
 
     private lateinit var mContext: Context
 
@@ -19,7 +25,7 @@ class WithCustomTestActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_with_custom_test)
+        setContentView(R.layout.activity_with_clickable_span)
 
         initComponents()
     }
@@ -40,11 +46,11 @@ class WithCustomTestActivity : AppCompatActivity() {
         tvOverview.movementMethod = LinkMovementMethod.getInstance()
         tvDescription.movementMethod = LinkMovementMethod.getInstance()
     }
-    
+
     private fun checkLineCountAndSetVisibility() {
         tvOverview.movementMethod = LinkMovementMethod.getInstance()
         tvOverview.post {
-            if(tvOverview.lineCount > 3) {
+            if (tvOverview.lineCount > 3) {
                 tvOverviewReadMoreHide.visibility = View.VISIBLE
             } else {
                 tvOverviewReadMoreHide.visibility = View.GONE
@@ -53,11 +59,23 @@ class WithCustomTestActivity : AppCompatActivity() {
 
         tvDescription.movementMethod = LinkMovementMethod.getInstance()
         tvDescription.post {
-            if(tvDescription.lineCount > 3) {
+            if (tvDescription.lineCount > 3) {
                 tvDescriptionReadMoreHide.visibility = View.VISIBLE
             } else {
                 tvDescriptionReadMoreHide.visibility = View.GONE
             }
         }
+
+
+        val ss = SpannableString("This is a sentence to check the lick which is clickable. ")
+        val clickableSpan: ClickableSpan = object : ClickableSpan() {
+            override fun onClick(textView: View) {
+                toast("Clicked")
+            }
+        }
+
+        ss.setSpan(clickableSpan, 0, 10, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        tvOverview.text = ss
     }
 }
